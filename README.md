1376b29fa2bb461f128e3ce2184ce18499cae487
Mateus Castro da Cunha <mateus.mc87@gmail.com>
2022-08-20 16:11:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# youtube-downloader-gui
+ Application to download videos or audio on YT
---
+++ youtube-downloader.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+
+print("Cole o link do video que deseja baixar:")
+ytLinVideo = input()
+
+ytVideo = YouTube(ytLinVideo)
+
+
+print('Titulo do video:', ytVideo.title)
+print('Autor do video:', ytVideo.author)
+
+print("Para baixar somente o audio digite 1")
+print("Para baixar o video digite 2")
+optionDownload = int(input())
+
+
+
+if optionDownload == 1:
+    videoDownload = ytVideo.streams.get_audio_only()
+    videoDownload.download('C:/Users/Mateus/Music')
+else:
+    videoDownload = ytVideo.streams.get_highest_resolution()    
+    videoDownload.download('C:/Users/Mateus/Videos')
+
+
+
